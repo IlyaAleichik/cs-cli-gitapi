1d0e8bdc280c037f8cdb42b45093645ad138eb11
IlyaAleichik <ilya.alejchik@outlook.com>
2022-03-17 23:43:10 +0300

    add deserialize rest json data

--- Program.cs
+++ Program.cs
@@ -1,47 +1,46 @@
 ﻿using System;
-using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-//using Newtonsoft.Json;
-using System.Text.Json;
+using CodeBeautify;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace netcore_cli_webapi_test
 {
     class Program
     {
-
         static async Task<string> MakeHTTPCall(Uri url, AuthenticationHeaderValue authHeader)
         {
-            var client = new HttpClient();
+            HttpClient client = new HttpClient();
             client.DefaultRequestHeaders.Authorization = authHeader;
-
-            var response = await client.GetAsync(url);
-
+            HttpResponseMessage response = await client.GetAsync(url);
             if (response.StatusCode == HttpStatusCode.Unauthorized)
             {
-                // Authorization header has been set, but the server reports that it is missing.
-                // It was probably stripped out due to a redirect.
-
-                var finalRequestUri = response.RequestMessage.RequestUri; // contains the final location after following the redirect.
-
-                if (finalRequestUri != url) // detect that a redirect actually did occur.
+                var finalRequestUri = response.RequestMessage.RequestUri; 
+                if (finalRequestUri != url)
                 {
-                    // If this is public facing, add tests here to determine if Url should be trusted
                     response = await client.GetAsync(finalRequestUri);
                 }
             }
-
             return await response.Content.ReadAsStringAsync();
         }
 
         static async Task Main(string[] args)
         {
-            string jsonValue = "";
-            Uri url = new Uri("https://gitlab.com/api/v4/users/IlyaAleichik/projects");
-            jsonValue = await MakeHTTPCall(url, new AuthenticationHeaderValue("Bearer", "glpat-xjg9fsxRP9gizt25TfYH"));
-            Console.WriteLine(jsonValue);
+            List<Root> myDeserializedClass;
+
+            Uri url = new Uri("https://gitlab.com/api/v4/users/IlyaAleichik/projects?&per_page=100");
+            string jsonValue = await MakeHTTPCall(url, new AuthenticationHeaderValue("Bearer", "glpat-xjg9fsxRP9gizt25TfYH"));
+       
+            myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(jsonValue);
+            int count = 0;
+            foreach (Root item in myDeserializedClass)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine(myDeserializedClass.Count);
         }
     }
 }
--- Root.cs
+++ Root.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-
 using System.Globalization;
+using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
@@ -10,145 +10,379 @@ namespace CodeBeautify
 
     public class Namespace
     {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string path { get; set; }
-        public string kind { get; set; }
-        public string full_path { get; set; }
-        public object parent_id { get; set; }
-        public string avatar_url { get; set; }
-        public string web_url { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("path")]
+        public string Path { get; set; }
+
+        [JsonPropertyName("kind")]
+        public string Kind { get; set; }
+
+        [JsonPropertyName("full_path")]
+        public string FullPath { get; set; }
+
+        [JsonPropertyName("parent_id")]
+        public object ParentId { get; set; }
+
+        [JsonPropertyName("avatar_url")]
+        public string AvatarUrl { get; set; }
+
+        [JsonPropertyName("web_url")]
+        public string WebUrl { get; set; }
     }
 
     public class Links
     {
-        public string self { get; set; }
-        public string issues { get; set; }
-        public string merge_requests { get; set; }
-        public string repo_branches { get; set; }
-        public string labels { get; set; }
-        public string events { get; set; }
-        public string members { get; set; }
+        [JsonPropertyName("self")]
+        public string Self { get; set; }
+
+        [JsonPropertyName("issues")]
+        public string Issues { get; set; }
+
+        [JsonPropertyName("merge_requests")]
+        public string MergeRequests { get; set; }
+
+        [JsonPropertyName("repo_branches")]
+        public string RepoBranches { get; set; }
+
+        [JsonPropertyName("labels")]
+        public string Labels { get; set; }
+
+        [JsonPropertyName("events")]
+        public string Events { get; set; }
+
+        [JsonPropertyName("members")]
+        public string Members { get; set; }
     }
 
     public class Owner
     {
-        public int id { get; set; }
-        public string username { get; set; }
-        public string name { get; set; }
-        public string state { get; set; }
-        public string avatar_url { get; set; }
-        public string web_url { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("state")]
+        public string State { get; set; }
+
+        [JsonPropertyName("avatar_url")]
+        public string AvatarUrl { get; set; }
+
+        [JsonPropertyName("web_url")]
+        public string WebUrl { get; set; }
     }
 
     public class ContainerExpirationPolicy
     {
-        public string cadence { get; set; }
-        public bool enabled { get; set; }
-        public int keep_n { get; set; }
-        public string older_than { get; set; }
-        public string name_regex { get; set; }
-        public object name_regex_keep { get; set; }
-        public DateTime next_run_at { get; set; }
+        [JsonPropertyName("cadence")]
+        public string Cadence { get; set; }
+
+        [JsonPropertyName("enabled")]
+        public bool Enabled { get; set; }
+
+        [JsonPropertyName("keep_n")]
+        public int KeepN { get; set; }
+
+        [JsonPropertyName("older_than")]
+        public string OlderThan { get; set; }
+
+        [JsonPropertyName("name_regex")]
+        public string NameRegex { get; set; }
+
+        [JsonPropertyName("name_regex_keep")]
+        public object NameRegexKeep { get; set; }
+
+        [JsonPropertyName("next_run_at")]
+        public DateTime NextRunAt { get; set; }
     }
 
     public class ProjectAccess
     {
-        public int access_level { get; set; }
-        public int notification_level { get; set; }
+        [JsonPropertyName("access_level")]
+        public int AccessLevel { get; set; }
+
+        [JsonPropertyName("notification_level")]
+        public int NotificationLevel { get; set; }
     }
 
     public class Permissions
     {
-        public ProjectAccess project_access { get; set; }
-        public object group_access { get; set; }
+        [JsonPropertyName("project_access")]
+        public ProjectAccess ProjectAccess { get; set; }
+
+        [JsonPropertyName("group_access")]
+        public object GroupAccess { get; set; }
     }
 
     public class Root
     {
-        public int id { get; set; }
-        public string description { get; set; }
-        public string name { get; set; }
-        public string name_with_namespace { get; set; }
-        public string path { get; set; }
-        public string path_with_namespace { get; set; }
-        public DateTime created_at { get; set; }
-        public string default_branch { get; set; }
-        public List<object> tag_list { get; set; }
-        public List<object> topics { get; set; }
-        public string ssh_url_to_repo { get; set; }
-        public string http_url_to_repo { get; set; }
-        public string web_url { get; set; }
-        public string readme_url { get; set; }
-        public object avatar_url { get; set; }
-        public int forks_count { get; set; }
-        public int star_count { get; set; }
-        public DateTime last_activity_at { get; set; }
-        public Namespace @namespace { get; set; }
-        public string container_registry_image_prefix { get; set; }
-        public Links _links { get; set; }
-        public bool packages_enabled { get; set; }
-        public bool empty_repo { get; set; }
-        public bool archived { get; set; }
-        public string visibility { get; set; }
-        public Owner owner { get; set; }
-        public bool resolve_outdated_diff_discussions { get; set; }
-        public ContainerExpirationPolicy container_expiration_policy { get; set; }
-        public bool issues_enabled { get; set; }
-        public bool merge_requests_enabled { get; set; }
-        public bool wiki_enabled { get; set; }
-        public bool jobs_enabled { get; set; }
-        public bool snippets_enabled { get; set; }
-        public bool container_registry_enabled { get; set; }
-        public bool service_desk_enabled { get; set; }
-        public string service_desk_address { get; set; }
-        public bool can_create_merge_request_in { get; set; }
-        public string issues_access_level { get; set; }
-        public string repository_access_level { get; set; }
-        public string merge_requests_access_level { get; set; }
-        public string forking_access_level { get; set; }
-        public string wiki_access_level { get; set; }
-        public string builds_access_level { get; set; }
-        public string snippets_access_level { get; set; }
-        public string pages_access_level { get; set; }
-        public string operations_access_level { get; set; }
-        public string analytics_access_level { get; set; }
-        public string container_registry_access_level { get; set; }
-        public object emails_disabled { get; set; }
-        public bool shared_runners_enabled { get; set; }
-        public bool lfs_enabled { get; set; }
-        public int creator_id { get; set; }
-        public string import_status { get; set; }
-        public int open_issues_count { get; set; }
-        public int ci_default_git_depth { get; set; }
-        public bool ci_forward_deployment_enabled { get; set; }
-        public bool ci_job_token_scope_enabled { get; set; }
-        public bool public_jobs { get; set; }
-        public int build_timeout { get; set; }
-        public string auto_cancel_pending_pipelines { get; set; }
-        public object build_coverage_regex { get; set; }
-        public string ci_config_path { get; set; }
-        public List<object> shared_with_groups { get; set; }
-        public bool only_allow_merge_if_pipeline_succeeds { get; set; }
-        public object allow_merge_on_skipped_pipeline { get; set; }
-        public bool restrict_user_defined_variables { get; set; }
-        public bool request_access_enabled { get; set; }
-        public bool only_allow_merge_if_all_discussions_are_resolved { get; set; }
-        public bool remove_source_branch_after_merge { get; set; }
-        public bool printing_merge_request_link_enabled { get; set; }
-        public string merge_method { get; set; }
-        public string squash_option { get; set; }
-        public object suggestion_commit_message { get; set; }
-        public object merge_commit_template { get; set; }
-        public object squash_commit_template { get; set; }
-        public bool auto_devops_enabled { get; set; }
-        public string auto_devops_deploy_strategy { get; set; }
-        public bool autoclose_referenced_issues { get; set; }
-        public bool keep_latest_artifact { get; set; }
-        public string external_authorization_classification_label { get; set; }
-        public bool requirements_enabled { get; set; }
-        public bool security_and_compliance_enabled { get; set; }
-        public List<object> compliance_frameworks { get; set; }
-        public Permissions permissions { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("name_with_namespace")]
+        public string NameWithNamespace { get; set; }
+
+        [JsonPropertyName("path")]
+        public string Path { get; set; }
+
+        [JsonPropertyName("path_with_namespace")]
+        public string PathWithNamespace { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonPropertyName("default_branch")]
+        public string DefaultBranch { get; set; }
+
+        [JsonPropertyName("tag_list")]
+        public List<object> TagList { get; set; }
+
+        [JsonPropertyName("topics")]
+        public List<object> Topics { get; set; }
+
+        [JsonPropertyName("ssh_url_to_repo")]
+        public string SshUrlToRepo { get; set; }
+
+        [JsonPropertyName("http_url_to_repo")]
+        public string HttpUrlToRepo { get; set; }
+
+        [JsonPropertyName("web_url")]
+        public string WebUrl { get; set; }
+
+        [JsonPropertyName("readme_url")]
+        public string ReadmeUrl { get; set; }
+
+        [JsonPropertyName("avatar_url")]
+        public object AvatarUrl { get; set; }
+
+        [JsonPropertyName("forks_count")]
+        public int ForksCount { get; set; }
+
+        [JsonPropertyName("star_count")]
+        public int StarCount { get; set; }
+
+        [JsonPropertyName("last_activity_at")]
+        public DateTime LastActivityAt { get; set; }
+
+        [JsonPropertyName("namespace")]
+        public Namespace Namespace { get; set; }
+
+        [JsonPropertyName("container_registry_image_prefix")]
+        public string ContainerRegistryImagePrefix { get; set; }
+
+        [JsonPropertyName("_links")]
+        public Links Links { get; set; }
+
+        [JsonPropertyName("packages_enabled")]
+        public bool PackagesEnabled { get; set; }
+
+        [JsonPropertyName("empty_repo")]
+        public bool EmptyRepo { get; set; }
+
+        [JsonPropertyName("archived")]
+        public bool Archived { get; set; }
+
+        [JsonPropertyName("visibility")]
+        public string Visibility { get; set; }
+
+        [JsonPropertyName("owner")]
+        public Owner Owner { get; set; }
+
+        [JsonPropertyName("resolve_outdated_diff_discussions")]
+        public bool ResolveOutdatedDiffDiscussions { get; set; }
+
+        [JsonPropertyName("container_expiration_policy")]
+        public ContainerExpirationPolicy ContainerExpirationPolicy { get; set; }
+
+        [JsonPropertyName("issues_enabled")]
+        public bool IssuesEnabled { get; set; }
+
+        [JsonPropertyName("merge_requests_enabled")]
+        public bool MergeRequestsEnabled { get; set; }
+
+        [JsonPropertyName("wiki_enabled")]
+        public bool WikiEnabled { get; set; }
+
+        [JsonPropertyName("jobs_enabled")]
+        public bool JobsEnabled { get; set; }
+
+        [JsonPropertyName("snippets_enabled")]
+        public bool SnippetsEnabled { get; set; }
+
+        [JsonPropertyName("container_registry_enabled")]
+        public bool ContainerRegistryEnabled { get; set; }
+
+        [JsonPropertyName("service_desk_enabled")]
+        public bool ServiceDeskEnabled { get; set; }
+
+        [JsonPropertyName("service_desk_address")]
+        public string ServiceDeskAddress { get; set; }
+
+        [JsonPropertyName("can_create_merge_request_in")]
+        public bool CanCreateMergeRequestIn { get; set; }
+
+        [JsonPropertyName("issues_access_level")]
+        public string IssuesAccessLevel { get; set; }
+
+        [JsonPropertyName("repository_access_level")]
+        public string RepositoryAccessLevel { get; set; }
+
+        [JsonPropertyName("merge_requests_access_level")]
+        public string MergeRequestsAccessLevel { get; set; }
+
+        [JsonPropertyName("forking_access_level")]
+        public string ForkingAccessLevel { get; set; }
+
+        [JsonPropertyName("wiki_access_level")]
+        public string WikiAccessLevel { get; set; }
+
+        [JsonPropertyName("builds_access_level")]
+        public string BuildsAccessLevel { get; set; }
+
+        [JsonPropertyName("snippets_access_level")]
+        public string SnippetsAccessLevel { get; set; }
+
+        [JsonPropertyName("pages_access_level")]
+        public string PagesAccessLevel { get; set; }
+
+        [JsonPropertyName("operations_access_level")]
+        public string OperationsAccessLevel { get; set; }
+
+        [JsonPropertyName("analytics_access_level")]
+        public string AnalyticsAccessLevel { get; set; }
+
+        [JsonPropertyName("container_registry_access_level")]
+        public string ContainerRegistryAccessLevel { get; set; }
+
+        [JsonPropertyName("security_and_compliance_access_level")]
+        public string SecurityAndComplianceAccessLevel { get; set; }
+
+        [JsonPropertyName("emails_disabled")]
+        public object EmailsDisabled { get; set; }
+
+        [JsonPropertyName("shared_runners_enabled")]
+        public bool SharedRunnersEnabled { get; set; }
+
+        [JsonPropertyName("lfs_enabled")]
+        public bool LfsEnabled { get; set; }
+
+        [JsonPropertyName("creator_id")]
+        public int CreatorId { get; set; }
+
+        [JsonPropertyName("import_status")]
+        public string ImportStatus { get; set; }
+
+        [JsonPropertyName("open_issues_count")]
+        public int OpenIssuesCount { get; set; }
+
+        [JsonPropertyName("ci_default_git_depth")]
+        public int CiDefaultGitDepth { get; set; }
+
+        [JsonPropertyName("ci_forward_deployment_enabled")]
+        public bool CiForwardDeploymentEnabled { get; set; }
+
+        [JsonPropertyName("ci_job_token_scope_enabled")]
+        public bool CiJobTokenScopeEnabled { get; set; }
+
+        [JsonPropertyName("public_jobs")]
+        public bool PublicJobs { get; set; }
+
+        [JsonPropertyName("build_timeout")]
+        public int BuildTimeout { get; set; }
+
+        [JsonPropertyName("auto_cancel_pending_pipelines")]
+        public string AutoCancelPendingPipelines { get; set; }
+
+        [JsonPropertyName("build_coverage_regex")]
+        public object BuildCoverageRegex { get; set; }
+
+        [JsonPropertyName("ci_config_path")]
+        public string CiConfigPath { get; set; }
+
+        [JsonPropertyName("shared_with_groups")]
+        public List<object> SharedWithGroups { get; set; }
+
+        [JsonPropertyName("only_allow_merge_if_pipeline_succeeds")]
+        public bool OnlyAllowMergeIfPipelineSucceeds { get; set; }
+
+        [JsonPropertyName("allow_merge_on_skipped_pipeline")]
+        public object AllowMergeOnSkippedPipeline { get; set; }
+
+        [JsonPropertyName("restrict_user_defined_variables")]
+        public bool RestrictUserDefinedVariables { get; set; }
+
+        [JsonPropertyName("request_access_enabled")]
+        public bool RequestAccessEnabled { get; set; }
+
+        [JsonPropertyName("only_allow_merge_if_all_discussions_are_resolved")]
+        public bool OnlyAllowMergeIfAllDiscussionsAreResolved { get; set; }
+
+        [JsonPropertyName("remove_source_branch_after_merge")]
+        public bool RemoveSourceBranchAfterMerge { get; set; }
+
+        [JsonPropertyName("printing_merge_request_link_enabled")]
+        public bool PrintingMergeRequestLinkEnabled { get; set; }
+
+        [JsonPropertyName("merge_method")]
+        public string MergeMethod { get; set; }
+
+        [JsonPropertyName("squash_option")]
+        public string SquashOption { get; set; }
+
+        [JsonPropertyName("suggestion_commit_message")]
+        public object SuggestionCommitMessage { get; set; }
+
+        [JsonPropertyName("merge_commit_template")]
+        public object MergeCommitTemplate { get; set; }
+
+        [JsonPropertyName("squash_commit_template")]
+        public object SquashCommitTemplate { get; set; }
+
+        [JsonPropertyName("auto_devops_enabled")]
+        public bool AutoDevopsEnabled { get; set; }
+
+        [JsonPropertyName("auto_devops_deploy_strategy")]
+        public string AutoDevopsDeployStrategy { get; set; }
+
+        [JsonPropertyName("autoclose_referenced_issues")]
+        public bool AutocloseReferencedIssues { get; set; }
+
+        [JsonPropertyName("keep_latest_artifact")]
+        public bool KeepLatestArtifact { get; set; }
+
+        [JsonPropertyName("runner_token_expiration_interval")]
+        public object RunnerTokenExpirationInterval { get; set; }
+
+        [JsonPropertyName("external_authorization_classification_label")]
+        public string ExternalAuthorizationClassificationLabel { get; set; }
+
+        [JsonPropertyName("requirements_enabled")]
+        public bool RequirementsEnabled { get; set; }
+
+        [JsonPropertyName("requirements_access_level")]
+        public string RequirementsAccessLevel { get; set; }
+
+        [JsonPropertyName("security_and_compliance_enabled")]
+        public bool SecurityAndComplianceEnabled { get; set; }
+
+        [JsonPropertyName("compliance_frameworks")]
+        public List<object> ComplianceFrameworks { get; set; }
+
+        [JsonPropertyName("permissions")]
+        public Permissions Permissions { get; set; }
     }
 }
